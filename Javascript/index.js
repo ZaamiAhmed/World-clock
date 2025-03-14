5d74bb66fdff7e63acff58990bfbc16d15f1a0ba
ZaamiAhmed <158285599+ZaamiAhmed@users.noreply.github.com>
2025-03-14 21:48:18 +0300

    added mapping to get citynames accordingly

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,26 +1,45 @@
 let selectedElement = document.querySelector("#time-element");
 
+const citySelectedTimezone={
+  "America/Denver": "Utah",
+  "Africa/Cairo":"Cairo",
+  "Europe/Paris":"Paris",
+  "Africa/Mogadishu":"Mogadishu",
+  "Africa/Lagos":"Lagos",
+  // "Africa/Nairobi": "Nairobi",
+  "Asia/Kolkata": "New Delhi",
+  "Europe/Lisbon":"Lisbon",
+ 
+
+}
+// I used the object above to mapp the city time zones because some timezones donot match the
+// expected city names given.
+
+
+
 function updateCity() {
   let watchelement = document.querySelector(".watch");
   let selectedTimezone = document.querySelector("#time-element").value;
   let cityelement = document.querySelector("#cities");
-  //  let cityDate=moment.tz(timeZone).format("ddd Do MMMM");
-  let year = moment().format("YYYY");
 
-  let selectedCity = selectedElement.value
-    ? selectedElement.selectedOptions[0].text
-    : "Nairobi";
- 
+if (!selectedTimezone || selectedTimezone==="Current") {
+  selectedTimezone=moment.tz.guess();
+  
+}
+let cityName=citySelectedTimezone[selectedTimezone] ||selectedTimezone.split("/")[1];
 
-  let timeZone = selectedTimezone ? selectedTimezone : moment.tz.guess();
-  let time = moment.tz(timeZone).format("h:mm:ss a");
-  let cityDate = moment.tz(timeZone).format("ddd Do MMMM");
 
+    // Format time and date using Moment.js
+  let time = moment.tz(selectedTimezone).format("h:mm:ss a");
+  let cityDate = moment.tz(selectedTimezone).format("ddd Do MMMM");
+ let year = moment.tz(selectedTimezone).format("YYYY");
   watchelement.innerHTML = time;
 
+
+// user interface.
   cityelement.innerHTML = `
       <div>
-      <h2 class="city">${selectedCity}</h2>
+      <h2 class="city">${cityName}</h2>
       <p class="date">${cityDate}</p>
       </div>
       <span class="year">${year}</span>
@@ -28,7 +47,7 @@ function updateCity() {
   
    `;
 }
-
+// Event listener for timezone selection
 selectedElement.addEventListener("change", updateCity);
 
 setInterval(updateCity, 1000);
